63e9ffddafef018e00360c9b344874529f375fa3
jair basas <jairbasas@outlook.com>
2023-01-05 22:10:34 -0500

    Fix, add tables

---
+++ CRGPDB/Tables/CONTRACT.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE EMPLOYEE.CONTRACT
+( 
+	employee_id          integer  NOT NULL ,
+	start_date           datetime  NULL ,
+	end_date             datetime  NULL ,
+	contract_type_id     varchar(20)  NULL ,
+	register_user_id     integer  NULL ,
+	register_user_fullname varchar(250)  NULL ,
+	register_datetime    datetime  NULL ,
+	update_user_id       integer  NULL ,
+	update_user_fullname varchar(250)  NULL ,
+	update_datetime      datetime  NULL 
+)
+go
+
+ALTER TABLE EMPLOYEE.CONTRACT
+	ADD CONSTRAINT PK_EMPLOYEE_CONTRACT PRIMARY KEY  CLUSTERED (employee_id ASC)
+go
+
+ALTER TABLE EMPLOYEE.CONTRACT
+	ADD CONSTRAINT FK_CONTRACT_EMPLOYEE FOREIGN KEY (employee_id) REFERENCES EMPLOYEES.EMPLOYEE(employee_id)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
