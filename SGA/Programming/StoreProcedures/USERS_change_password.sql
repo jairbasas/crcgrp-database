f57a8dec6ea3ee983863de77896f6326f59cb5d8
jair basas <jairbasas@outlook.com>
2023-01-07 07:05:17 -0500

    Fix, add sp´s

---
+++ SGA/Programming/StoreProcedures/USERS_change_password.sql
@@ -0,0 +1,21 @@
+﻿Create Procedure SECURITY.USERS_change_password
+(
+  @pii_user_id INT,
+  @piv_password VARCHAR(200),
+  @pii_reset_password INT,
+  @pii_update_user_id INT,
+  @piv_update_user_fullname VARCHAR(250),
+  @pid_update_datetime DATETIME
+)
+AS
+BEGIN
+
+  UPDATE SECURITY.users 
+  SET password = ENCRYPTBYPASSPHRASE('password', @piv_password)
+	 ,reset_password = @pii_reset_password
+     ,update_user_id = @pii_update_user_id
+     ,update_user_fullname = @piv_update_user_fullname
+     ,update_datetime = @pid_update_datetime
+  WHERE user_id = @pii_user_id;  
+
+END
---
+++ SGA/Programming/StoreProcedures/USERS_change_state.sql
@@ -0,0 +1,19 @@
+﻿Create Procedure SECURITY.USERS_change_state
+(
+	@pii_user_id int,
+	@pii_state int,
+	@pii_update_user_id int,
+	@piv_update_user_fullname varchar(250),
+	@pid_update_datetime datetime
+)
+AS
+BEGIN
+	
+	UPDATE SECURITY.users
+		SET state = @pii_state,
+			update_user_id = @pii_update_user_id,
+			update_user_fullname = @piv_update_user_fullname,
+			update_datetime = @pid_update_datetime
+	WHERE user_id = @pii_user_id
+
+END;
